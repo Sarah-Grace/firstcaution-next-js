db62a3d56153bec3b622cab2823bc3f666d86415
Sarah-Grace <sarah.grace4875@gmail.com>
2024-11-26 15:09:43 +0500

    Request Debt Enforcement Certificate Form creation

--- src/app/(main)/firstmoove/page.jsx
+++ src/app/(main)/firstmoove/page.jsx
@@ -57,7 +57,7 @@ function Firstmoove() {
                       </AccordionItem>
                     </Accordion>
                   </div> */}
-                 <div className="flex items-center gap-6 xs:gap-2">
+                <div className="flex items-center gap-6 xs:gap-2">
                   <Image
                       src="/images/icons/tenant-doc.png"
                       alt=""
@@ -72,7 +72,46 @@ function Firstmoove() {
                 </div>
               </TabsContent>
               <TabsContent key={tabNames[1]} value={tabNames[1]}>
-
+                <div className="flex items-center gap-6 xs:gap-2">
+                  <Image
+                      src="/images/icons/tenant-doc.png"
+                      alt=""
+                      className="xs:w-10 xs:h-10"
+                      width={55}
+                      height={55}
+                    />
+                    <div>
+                      <h2 className="text-h2 font-medium mb-2 text-content xs:mb-0 xs:text-base">Automate the request for a certificate</h2>
+                      <h3 className="text-h3 font-medium text-grey-2 mb-2 xs:mb-0">Submit your request and we’ll handle the rest.</h3>
+                    </div>
+                </div>
+                <form>
+                  <div className="flex gap-7">
+                      <div className="w-2/5 xxl:w-1/2 tablet:w-full">
+                        <div className="w-full mb-6">
+                          <label className="text-base font-normal text-content leading-5 mb-3 block">Your Name</label>
+                          <input 
+                            className={`leading-[48px] py-0 px-5 pr-[30px] xs:pr-[5px] text-[15px] font-normal text-[#909090] bg-transparent border border-[#DFEAF2] focus-visible:outline-none rounded-8 w-full`}
+                            value="Charlene Reed"
+                            readOnly
+                          />
+                        </div>
+                        <div className="w-full mb-6">
+                          <label className="text-base font-normal text-content leading-5 mb-3 block">Your Address</label>
+                          <input 
+                            className={`leading-[48px] py-0 px-5 pr-[30px] xs:pr-[5px] text-[15px] font-normal text-[#909090] bg-transparent border border-[#DFEAF2] focus-visible:outline-none rounded-8 w-full`}
+                            value="2715 Ash Dr. San Jose, South Dakota 83475"
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                  </div>
+                  <div className="flex justify-end mt-12 md1:justify-center">
+                      <button className="rounded-8 bg-secondary text-white py-4 px-[60px] border-0 block leading-4">
+                          Submit
+                      </button>
+                  </div>
+                </form>
               </TabsContent>
               <TabsContent key={tabNames[2]} value={tabNames[2]}>
                 <div className="flex items-center gap-6 xs:gap-2">
