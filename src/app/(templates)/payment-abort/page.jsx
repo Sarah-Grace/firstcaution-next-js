206de557ddee9a6e097cce5e8d2f2fbb75df060f
Sarah-Grace <sarah.grace4875@gmail.com>
2024-12-23 14:40:25 +0500

    post message from payment-abort age

--- src/app/(templates)/payment-abort/page.jsx
+++ src/app/(templates)/payment-abort/page.jsx
@@ -1,15 +1,28 @@
+'use client'
+import { useEffect } from 'react';
 import styles from './Abort.module.css';
 
 function Success() {
+  useEffect(() => {
+    // Notify parent about the URL change
+    const notifyParent = () => {
+      const currentPath = window.location.pathname;
+
+      let status = 'success';
+      // Post the message to the parent
+      window.parent.postMessage({ status, path: currentPath }, '*');
+    };
+
+    // Detect URL changes (SPA or regular navigation)
+    window.addEventListener('popstate', notifyParent); // Back/forward navigation
+    notifyParent('abort')
+  }, [])
   return (
     <div className={styles.main}>
       <div className={styles.messageBlock}>
         <div className={styles.xMark}>X</div>
         <h1 className={styles.h1}>Your payment has been aborted</h1>
         <p className={styles.p}>Payment aborted. Please retry or verify your payment details.</p>
-        {/* <a href="#" className={styles.button}>
-          Go to Dashboard
-        </a> */}
       </div>
     </div>
   )
